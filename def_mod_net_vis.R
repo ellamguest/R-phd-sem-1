6359a9ca932fd7482c8bb7aebf91a864be5c5a17
ellamguest <ellamguest@gmail.com>
2016-10-25 11:50:57 +0100

    fixing def mod net visualisations

--- def_mod_net_vis.R
+++ def_mod_net_vis.R
@@ -3,67 +3,71 @@
 library("igraph")
 
 ### IMPORT DATA
-# partial_edges = read.csv('edgelist.csv', header=T)
-# partial_nodes = read.csv('nodelist.csv', header=T)
-edges = read.csv('edgelist_full.csv', header=T)
-nodes = read.csv('nodelist_full.csv', header=T)
+partial_edges = read.csv('edgelist.csv', header=T)
+partial_nodes = read.csv('nodelist.csv', header=T)
+# edges = read.csv('edgelist_full.csv', header=T)
+# nodes = read.csv('nodelist_full.csv', header=T)
 
 
 # creating network objects
 net <- graph_from_data_frame(d=edges, vertices=nodes)
+net <- graph_from_data_frame(d=partial_edges, vertices=partial_nodes)
 
 
 ### ADDING NODE ATTIBUTES
-# 1) network measure attributes
-# V(net)$degree <- degree(net, mode="all")
-# V(net)$between <- betweenness(net) # 0 for all
-# V(net)$closeness<- closeness(net)
-
-# 2) network visualisation attributesattributes
+# network visualisation attributesattributes
 colrs <- c("red", "blue")
-V(net)$color <- colrs[V(net)$type+1] # red = redditors (mod), blue = subs
+# V(net)$color <- colrs[V(net)$type+1] # red = redditors (mod), blue = subs
+V(net)$color = adjustcolor(colrs[V(net)$type+1], 0.5)
 V(net)$shape <- c("square", "circle")[V(net)$type+1]  # square = redditors (mod), circle = subs
 V(net)$label.font = V(net)$type+1 # redditors = plain, subs = bold
 V(net)$label.cex = (V(net)$type+1)*0.5
 
 ### SELECTING FOR LARGE COMPONETS
 clu = components(net, mode = c("weak", "strong"))
-groups(clu) #### MUST CUSTOMISE HERE FOR DIFFERENT NETWORKS
 
+#### MUST CUSTOMISE HERE FOR DIFFERENT NETWORKS
 # for orgs partial network:
-# c1 = mapply(c, groups(clu)[1],SIMPLIFY=FALSE)
-# x = unlist(conn)
-# sub = induced_subgraph(net,x)
 
-# for full network:
-sub = induced_subgraph(net,unlist(groups(clu)[1]))
-sub2 = induced_subgraph(net,unlist(groups(clu)[2]))
-sub3 = induced_subgraph(net,unlist(groups(clu)[3]))
 
-### PLOT NETWORKS
+### PLOT ORIGINAL MODERATORS NETWORK
+# # orginial moderators network components:
+giant = mapply(c, groups(clu)[1], groups(clu)[7], SIMPLIFY=FALSE)
+sub = induced_subgraph(net, unlist(giant))
+
+loose = mapply(c, groups(clu)[2:6], groups(clu)[8:23], SIMPLIFY=FALSE)
+sub2 = induced_subgraph(net, unlist(loose))
+
+
 # node shape plot
-pdf('?.pdf')
-plot(sub2, vertex.label = V(sub)$label, vertex.size=1, edge.arrow.size=0)
+pdf('orgs_loose.pdf')
+plot(sub2, vertex.label.color='black', vertex.frame.color=NA,
+     vertex.size=5, edge.arrow.size=0)
 dev.off()
 
-# plot giant component
-# setting labels for subreddits only
-V(sub)$label[V(sub)$type==0] <- ""
-V(sub)$label[V(sub)$type==1] = V(sub)$name[V(sub)$type==1]
-V(sub)$label.cex = 1
-V(sub)$label.font= 1
-V(sub)$label.color = 'black'
+## PLOTTING FULL NETWORK
+# # full network components:
+# sub = induced_subgraph(net,unlist(groups(clu)[1]))
+# sub2 = induced_subgraph(net,unlist(groups(clu)[2]))
+# sub3 = induced_subgraph(net,unlist(groups(clu)[3]))
 
-l <- layout_with_fr(sub) # best layout for orgs net
-
+# # plot full giant component
+# # setting labels for subreddits only
+# V(sub)$label[V(sub)$type==0] <- ""
+# V(sub)$label[V(sub)$type==1] = V(sub)$name[V(sub)$type==1]
+# V(sub)$label.cex = 1
+# V(sub)$label.font= 1
+# V(sub)$label.color = 'black'
+# V(sub)$degree <- degree(sub, mode="all")
+# 
 # pdf('giantcomponent.pdf')
 # plot(sub, vertex.shape = 'none', edge.arrow.size=0, layout=l,
-#      vertex.label.cex=log(V(sub)$degree))
+#      vertex.label.cex=1)
 # dev.off()
-
-# plot small components
-plot(sub2, vertex.shape = 'none', edge.arrow.size=0, layout=l,
-     vertex.label.cex=1, vertex.label.color=V(sub2)$color)
-
-plot(sub3, vertex.shape = 'none', edge.arrow.size=0, layout=layout_with_fr,
-     vertex.label.cex=1, vertex.label.color=V(sub3)$color)
+# 
+# # plot full net small components
+# plot(sub2, vertex.shape = 'none', edge.arrow.size=0, layout=layout_with_fr,
+#      vertex.label.cex=1, vertex.label.color=V(sub2)$color)
+# 
+# plot(sub3, vertex.shape = 'none', edge.arrow.size=0, layout=layout_with_fr,
+#      vertex.label.cex=1, vertex.label.color=V(sub3)$color)
